f19ea263f634f4feae87660c4d3a68b6f34179f5
mlabecki <3i14159265@gmail.com>
2024-09-19 11:16:20 -0400

    Renamed a downloads config file

--- src/download_data.py
+++ src/download_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import requests
-from mapping_portfolio_optimization import *
+from mapping_portfolio_downloads import *
 from mapping_tickers import *
 
 class DownloadData():
