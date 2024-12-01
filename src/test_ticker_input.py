00370cc23a8ca7fbbe181af8314dd59fa0f7211c
mlabecki <3i14159265@gmail.com>
2024-12-01 15:22:04 -0500

    Generalized creation of input tables from fixed ticker lists in different categories

--- src/test_ticker_input.py
+++ src/test_ticker_input.py
@@ -36,7 +36,7 @@ tk_market = '^GSPC'
 # 'nasdaq100', 'sp500', 'dow_jones', 'biggest_companies',
 # 'biggest_etfs', 'crypto_etfs', 'cryptos', 'cryptos_coin360', 'futures'
 
-max_tickers = 5
+max_tickers = 7
 # ticker_category = 'crypto_etfs'
 # ticker_category = 'biggest_companies'
 # ticker_category = 'cryptos'
@@ -58,76 +58,251 @@ tickers_risk_free_treasury = list(risk_free_treasury_tickers.keys())
 tickers_volatility_indices = list(volatility_tickers.keys())
 tickers_currency_etfs = list(currency_etf_tickers.keys())
 
-input_table_columns = ['No.', 'Ticker', 'Name', 'Data Start', 'Data End', 'Type', 'Exchange', 'Currency']
-custom_ticker_table_columns = ['Ticker', 'Name', 'Data Start', 'Data End', 'Type', 'Exchange', 'Currency']
+input_table_columns_indices = ['No.', 'Ticker', 'Name', 'Data Start', 'Data End', 'Exchange', 'Currency']
+input_table_columns_futures = ['No.', 'Ticker', 'Name', 'Data Start', 'Data End', 'Exchange', 'Currency']
+input_table_columns_equities = ['No.', 'Ticker', 'Name', 'Data Start', 'Data End', 'Industry', 'Sector', 'Exchange', 'Currency']
+input_table_columns_etfs = ['No.', 'Ticker', 'Name', 'Data Start', 'Data End', 'Category', 'Exchange', 'Currency']
 
-# df_ticker_info = pd.DataFrame(index = df['Symbol'], columns = input_table_columns)
-df_info_tickers_bond_etfs = pd.DataFrame(index = tickers_bond_etfs, columns = input_table_columns)
-df_info_tickers_stock_indices = pd.DataFrame(index = tickers_stock_indices, columns = input_table_columns)
-df_info_tickers_magnificent_seven = pd.DataFrame(index = tickers_magnificent_seven, columns = input_table_columns)
-df_info_tickers_commodity_etfs = pd.DataFrame(index = tickers_commodity_etfs, columns = input_table_columns)
-df_info_tickers_crypto_benchmarks = pd.DataFrame(index = tickers_crypto_benchmarks, columns = input_table_columns)
-df_info_tickers_precious_metal_futures = pd.DataFrame(index = tickers_precious_metal_futures, columns = input_table_columns)
-df_info_tickers_risk_free_treasury = pd.DataFrame(index = tickers_risk_free_treasury, columns = input_table_columns)
-df_info_tickers_volatility_indices = pd.DataFrame(index = tickers_volatility_indices, columns = input_table_columns)
-df_info_tickers_currency_etfs = pd.DataFrame(index = tickers_currency_etfs, columns = input_table_columns)
+custom_ticker_table_columns_indices = ['Ticker', 'Name', 'Data Start', 'Data End', 'Type', 'Exchange', 'Currency']
+custom_ticker_table_columns_futures = ['Ticker', 'Name', 'Data Start', 'Data End', 'Type', 'Exchange', 'Currency']
+custom_ticker_table_columns_equities = ['Ticker', 'Name', 'Data Start', 'Data End', 'Type', 'Industry', 'Sector', 'Exchange', 'Currency']
+custom_ticker_table_columns_etfs = ['Ticker', 'Name', 'Data Start', 'Data End', 'Type', 'Category', 'Exchange', 'Currency']
+
+df_info_tickers_bond_etfs = pd.DataFrame(index = tickers_bond_etfs, columns = input_table_columns_etfs)
+df_info_tickers_stock_indices = pd.DataFrame(index = tickers_stock_indices, columns = input_table_columns_indices)
+df_info_tickers_magnificent_seven = pd.DataFrame(index = tickers_magnificent_seven, columns = input_table_columns_equities)
+df_info_tickers_commodity_etfs = pd.DataFrame(index = tickers_commodity_etfs, columns = input_table_columns_etfs)
+df_info_tickers_crypto_benchmarks = pd.DataFrame(index = tickers_crypto_benchmarks, columns = input_table_columns_etfs)
+df_info_tickers_precious_metal_futures = pd.DataFrame(index = tickers_precious_metal_futures, columns = input_table_columns_futures)
+df_info_tickers_risk_free_treasury = pd.DataFrame(index = tickers_risk_free_treasury, columns = input_table_columns_indices)
+df_info_tickers_volatility_indices = pd.DataFrame(index = tickers_volatility_indices, columns = input_table_columns_indices)
+df_info_tickers_currency_etfs = pd.DataFrame(index = tickers_currency_etfs, columns = input_table_columns_etfs)
+
+row_ticker_map_bond_etfs = {}
+row_ticker_map_stock_indices = {}
+row_ticker_map_magnificent_seven = {}
+row_ticker_map_commodity_etfs = {}
+row_ticker_map_crypto_benchmarks = {}
+row_ticker_map_precious_metal_futures = {}
+row_ticker_map_risk_free_treasury = {}
+row_ticker_map_volatility_indices = {}
+row_ticker_map_currency_etfs = {}
+
+ticker_category_info_map = {
+    'bond_etfs': {
+        'df': df_info_tickers_bond_etfs,
+        'row': row_ticker_map_bond_etfs,
+        'dict': bond_etf_tickers
+    },
+    'stock_indices': {
+        'df': df_info_tickers_stock_indices,
+        'row': row_ticker_map_stock_indices,
+        'dict': stock_index_tickers
+    },
+    'magnificent_seven': {
+        'df': df_info_tickers_magnificent_seven,
+        'row': row_ticker_map_magnificent_seven,
+        'dict': magnificent_7_tickers
+    },
+    'commodity_etfs': {
+        'df': df_info_tickers_commodity_etfs,
+        'row': row_ticker_map_commodity_etfs,
+        'dict': commodity_etf_tickers
+    },
+    'crypto_benchmarks': {
+        'df': df_info_tickers_crypto_benchmarks,
+        'row': row_ticker_map_crypto_benchmarks,
+        'dict': crypto_benchmark_tickers
+    },
+    'precious_metal_futures': {
+        'df': df_info_tickers_precious_metal_futures,
+        'row': row_ticker_map_precious_metal_futures,
+        'dict': precious_metal_futures
+    },
+    'risk_free_treasury': {
+        'df': df_info_tickers_risk_free_treasury,
+        'row': row_ticker_map_risk_free_treasury,
+        'dict': risk_free_treasury_tickers
+    },
+    'volatility_indices': {
+        'df': df_info_tickers_volatility_indices,
+        'row': row_ticker_map_volatility_indices,
+        'dict': volatility_tickers
+    },
+    'currency_etfs': {
+        'df': df_info_tickers_currency_etfs,
+        'row': row_ticker_map_currency_etfs,
+        'dict': currency_etf_tickers
+    }
+}
 
 # ticker_menu_info = {}
 ticker_info = {}  # To help user decide on tickers based on the name and data start and end dates
 
-row_ticker_map_bond_etfs = {}
-for i, tk in enumerate(tickers_bond_etfs):
+def create_input_table(category):
+
+    df_info_tickers = ticker_category_info_map[category]['df']
+    row_ticker_map = ticker_category_info_map[category]['row']
+    dict_info_tickers = ticker_category_info_map[category]['dict']
+    category_tickers = list(dict_info_tickers.keys())
+
+    for i, tk in enumerate(category_tickers):
+
+        if tk not in ticker_info.keys():
+            
+            tk_hist = yf.Ticker(tk).history(period = 'max')
+            tk_info = yf.Ticker(tk).info
+            # Should also check if ticker is still valid, for now assume they're all valid
+
+            if len(tk_hist.index) > 0:
+
+                tk_start, tk_end = str(tk_hist.index[0].date()), str(tk_hist.index[-1].date())
+
+                df_info_tickers.at[tk, 'No.'] = i + 1
+                df_info_tickers.at[tk, 'Ticker'] = tk
+
+                if 'longName' in tk_info.keys():
+                    tk_name = tk_info['longName']
+                elif 'shortName' in tk_info.keys():
+                    tk_name = tk_info['shortName']
+                else:
+                    tk_name = dict_info_tickers[tk]
+
+                tk_type = tk_info['quoteType'] if 'quoteType' in tk_info.keys() else ''
+                tk_exchange = tk_info['exchange'] if 'exchange' in tk_info.keys() else ''
+                tk_currency = tk_info['currency'] if 'currency' in tk_info.keys() else ''
+                tk_category = tk_info['category'] if 'category' in tk_info.keys() else ''
+                tk_industry = tk_info['industry'] if 'industry' in tk_info.keys() else ''
+                tk_sector = tk_info['sector'] if 'sector' in tk_info.keys() else ''
+
+                if 'longBusinessSummary' in tk_info.keys():
+                    tk_summary = tk_info['longBusinessSummary']
+                elif 'description' in tk_info.keys():
+                    tk_summary = tk_info['description']
+                else: 
+                    tk_summary = ''
+
+                df_info_tickers.at[tk, 'Name'] = tk_name
+                df_info_tickers.at[tk, 'Data Start'] = tk_start
+                df_info_tickers.at[tk, 'Data End'] = tk_end
+                df_info_tickers.at[tk, 'Exchange'] = tk_exchange
+                df_info_tickers.at[tk, 'Currency'] = tk_currency
+
+                if tk_type == 'ETF':
+                    df_info_tickers.at[tk, 'Category'] = tk_category
+                elif tk_type == 'EQUITY':
+                    df_info_tickers.at[tk, 'Industry'] = tk_industry
+                    df_info_tickers.at[tk, 'Sector'] = tk_sector
+
+                row_ticker_map.update({tk: i})
+
+                ticker_info.update({
+                    tk: {
+                        'name': tk_name,
+                        'start': tk_start,
+                        'end': tk_end,
+                        'type': tk_type,
+                        'exchange': tk_exchange,
+                        'currency': tk_currency,
+                        'category': tk_category,
+                        'industry': tk_industry,
+                        'sector': tk_sector,
+                        'summary': tk_summary
+                    }
+                })
+
+            else:
+                print(f'WARNING: Cannot get data for {tk} at the moment, try again later')
     
-    yf_tk_hist = yf.Ticker(tk).history(period = 'max')
-    yf_tk_info = yf.Ticker(tk).info
-
-    if len(yf_tk_hist.index) > 0:
-
-        tk_start, tk_end = str(yf_tk_hist.index[0].date()), str(yf_tk_hist.index[-1].date())
-
-        df_info_tickers_bond_etfs.at[tk, 'No.'] = i + 1
-        df_info_tickers_bond_etfs.at[tk, 'Ticker'] = tk
-
-        if 'longName' in yf_tk_info.keys():
-            tk_name = yf_tk_info['longName']
-        elif 'shortName' in yf_tk_info.keys():
-            tk_name = yf_tk_info['shortName']
         else:
-            tk_name = bond_etf_tickers[tk]
 
-        tk_type = yf_tk_info['quoteType'] if 'quoteType' in yf_tk_info.keys() else ''
-        tk_exchange = yf_tk_info['exchange'] if 'exchange' in yf_tk_info.keys() else ''
-        tk_currency = yf_tk_info['currency'] if 'currency' in yf_tk_info.keys() else ''
-        if 'longBusinessSummary' in yf_tk_info.keys():
-            tk_summary = yf_tk_info['longBusinessSummary']
-        elif 'description' in yf_tk_info.keys():
-            tk_summary = yf_tk_info['description']
-        else: 
-            tk_summary = ''
+            df_info_tickers.at[tk, 'No.'] = i + 1
+            df_info_tickers.at[tk, 'Ticker'] = tk
+            df_info_tickers.at[tk, 'Name'] = ticker_info[tk]['name']
+            df_info_tickers.at[tk, 'Data Start'] = ticker_info[tk]['start']
+            df_info_tickers.at[tk, 'Data End'] = ticker_info[tk]['end']
+            df_info_tickers.at[tk, 'Exchange'] = ticker_info[tk]['exchange']
+            df_info_tickers.at[tk, 'Currency'] = ticker_info[tk]['currency']
+            if tk_type == 'ETF':
+                df_info_tickers.at[tk, 'Category'] = ticker_info[tk]['category']
+            elif tk_type == 'EQUITY':
+                df_info_tickers.at[tk, 'Industry'] = ticker_info[tk]['industry']
+                df_info_tickers.at[tk, 'Sector'] = ticker_info[tk]['sector']
 
-        df_info_tickers_bond_etfs.at[tk, 'Name'] = tk_name
-        df_info_tickers_bond_etfs.at[tk, 'Data Start'] = tk_start
-        df_info_tickers_bond_etfs.at[tk, 'Data End'] = tk_end
-        df_info_tickers_bond_etfs.at[tk, 'Type'] = tk_type
-        df_info_tickers_bond_etfs.at[tk, 'Exchange'] = tk_exchange
-        df_info_tickers_bond_etfs.at[tk, 'Currency'] = tk_currency
+        input_table_data = {
+            'df': df_info_tickers,
+            'row': row_ticker_map
+        }
 
-        row_ticker_map_bond_etfs.update({tk: i})
+    return input_table_data
 
-        ticker_info.update({
-            tk: {
-                'name': tk_name,
-                'start': tk_start,
-                'end': tk_end,
-                'type': tk_type,
-                'exchange': tk_exchange,
-                'currency': tk_currency,
-                'summary': tk_summary
-            }
-        })
 
-    else:
-        print(f'WARNING: Cannot get data for {tk} at the moment, try again later')
+for category in ticker_category_info_map.keys():
+    print(f'\nProcessing info for {category} ...')
+    input_table_data = create_input_table(category)
+    ticker_category_info_map[category]['df'] = input_table_data['df']
+    ticker_category_info_map[category]['row'] = input_table_data['row']
+    print(ticker_category_info_map[category]['df'])
+    print(ticker_category_info_map[category]['row'])
+
+
+print(df_info_tickers_magnificent_seven)
+
+import sys
+sys.exit()
+
+# for i, tk in enumerate(tickers_bond_etfs):
+#     
+#     yf_tk_hist = yf.Ticker(tk).history(period = 'max')
+#     yf_tk_info = yf.Ticker(tk).info
+# 
+#     if len(yf_tk_hist.index) > 0:
+# 
+#         tk_start, tk_end = str(yf_tk_hist.index[0].date()), str(yf_tk_hist.index[-1].date())
+# 
+#         df_info_tickers_bond_etfs.at[tk, 'No.'] = i + 1
+#         df_info_tickers_bond_etfs.at[tk, 'Ticker'] = tk
+# 
+#         if 'longName' in yf_tk_info.keys():
+#             tk_name = yf_tk_info['longName']
+#         elif 'shortName' in yf_tk_info.keys():
+#             tk_name = yf_tk_info['shortName']
+#         else:
+#             tk_name = bond_etf_tickers[tk]
+# 
+#         tk_type = yf_tk_info['quoteType'] if 'quoteType' in yf_tk_info.keys() else ''
+#         tk_exchange = yf_tk_info['exchange'] if 'exchange' in yf_tk_info.keys() else ''
+#         tk_currency = yf_tk_info['currency'] if 'currency' in yf_tk_info.keys() else ''
+#         if 'longBusinessSummary' in yf_tk_info.keys():
+#             tk_summary = yf_tk_info['longBusinessSummary']
+#         elif 'description' in yf_tk_info.keys():
+#             tk_summary = yf_tk_info['description']
+#         else: 
+#             tk_summary = ''
+# 
+#         df_info_tickers_bond_etfs.at[tk, 'Name'] = tk_name
+#         df_info_tickers_bond_etfs.at[tk, 'Data Start'] = tk_start
+#         df_info_tickers_bond_etfs.at[tk, 'Data End'] = tk_end
+#         df_info_tickers_bond_etfs.at[tk, 'Type'] = tk_type
+#         df_info_tickers_bond_etfs.at[tk, 'Exchange'] = tk_exchange
+#         df_info_tickers_bond_etfs.at[tk, 'Currency'] = tk_currency
+# 
+#         row_ticker_map_bond_etfs.update({tk: i})
+# 
+#         ticker_info.update({
+#             tk: {
+#                 'name': tk_name,
+#                 'start': tk_start,
+#                 'end': tk_end,
+#                 'type': tk_type,
+#                 'exchange': tk_exchange,
+#                 'currency': tk_currency,
+#                 'summary': tk_summary
+#             }
+#         })
+# 
+#     else:
+#         print(f'WARNING: Cannot get data for {tk} at the moment, try again later')
 
 # print(df_info_tickers_bond_etfs)
 
