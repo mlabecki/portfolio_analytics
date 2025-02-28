cfaa6a8e0dff4e5bafe7987e79829283483d457b
mlabecki <3i14159265@gmail.com>
2025-02-27 20:38:04 -0500

    Testing display of categories of interest on page 1

--- src/pages/page1.py
+++ src/pages/page1.py
@@ -19,11 +19,59 @@ register_page(
     path = '/'
 )
 
+category_option_css = {
+    'display': 'block',
+    'font-size': '14px',
+    'font-weight': 'bold',
+    'color': '#007ea7',  # This is native for the YETI theme
+    'vertical-align': 'middle',
+    'height': '30px',
+    'margin-top': '0px',
+    'margin-bottom': '2px',
+    'margin-left': '10px',
+    'padding-left': '10px',
+    'padding-top': '3px',
+    'border': '2px solid #007ea7'
+}
+
 layout = html.Div([
 
-    dcc.Link('Go to Preliminary Ticker Selection', href='/preliminary_ticker_selection_v3'),
-    html.Br(),
-    dcc.Link('Go to Ticker Info & Portfolio Selection', href='/test_ticker_input_v3'),
+    html.Div(
+        id = 'category-options-container',
+        children = [
+            html.Div(
+                'BIGGEST COMPANIES',
+                id = 'category-option-biggest-companies',
+                style = category_option_css
+            ),
+            html.Div(
+                'S&P 500 COMPANIES',
+                id = 'category-option-sp500',
+                style = category_option_css
+            ),
+            html.Div(
+                'NASDAQ 100 COMPANIES',
+                id = 'category-option-nasdaq100',
+                style = category_option_css
+            )
+        ],
+        style = {
+            'width': '310px',
+            'margin-top': '10px',
+            'margin-left': '10px',
+            'padding-left': '10px',
+        }
+    ),
+
+    html.Div(
+        id = 'dates-link-container',
+        children = [
+            dcc.Link('Go to Preliminary Ticker Selection', href='/preliminary_ticker_selection_v3'),
+            html.Br(),
+            # dcc.Link('Go to Ticker Info & Portfolio Selection', href='/test_ticker_input_v3'),
+        ],
+        style = link_container_css
+    )
 
     # html.Div(id = 'test-output-div')
 
